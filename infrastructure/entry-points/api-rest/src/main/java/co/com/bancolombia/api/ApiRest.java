702b888b0ac5cca885047cf8005056a238fa925d
Giovany Villegas <givilleg@bancolombia.com.co>
2021-01-15 05:44:27 -0500

    Se ajusta logger y depura clases

--- infrastructure/entry-points/api-rest/src/main/java/co/com/bancolombia/api/ApiRest.java
+++ infrastructure/entry-points/api-rest/src/main/java/co/com/bancolombia/api/ApiRest.java
@@ -3,6 +3,7 @@ package co.com.bancolombia.api;
 import co.com.bancolombia.model.saldo.Saldo;
 import co.com.bancolombia.usecase.calculorendimiento.CalculoRendimientoUseCase;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,14 +13,15 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
+@Slf4j
 public class ApiRest {
     private final CalculoRendimientoUseCase  useCase;
 
     @PostMapping(path ="/balance")
     public ResponseEntity<String> calcularBalance(@RequestBody List<Saldo> saldos){
 
-        saldos.stream().forEach(System.out::println);
-        Boolean esRentable = useCase.esRestableSaldoMes(saldos);
+        saldos.forEach(element -> log.info(String.valueOf(element)));
+        boolean esRentable = useCase.esRestableSaldoMes(saldos);
         if (esRentable) {
             return ResponseEntity.ok("Es rentable");
         }
